8ec86e2d716cc805cf60e676b239ed6efade19ff
chrysn <chrysn@fsfe.org>
2011-10-01 14:08:11 +0200

    added gpl copyright headers to all source files
    redhat needs it for inclusion[1], and it's preferred in debian anyway,
    so why not...
    
    [1] https://bugzilla.redhat.com/show_bug.cgi?id=739088

--- screenlayout/meta.py
+++ screenlayout/meta.py
@@ -1,4 +1,21 @@
 # This Python file uses the following encoding: utf-8
+
+# ARandR -- Another XRandR GUI
+# Copyright (C) 2008 -- 2011 chrysn <chrysn@fsfe.org>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import gettext
 gettext.install('arandr')
 
@@ -8,7 +25,7 @@ PROGRAMNAME = _(u'ARandR Screen Layout Editor')
 ## (ein weiteres GUI für XRandR)" so users get both the explanation of
 ## the acronym and a localized version.
 PROGRAMDESCRIPTION = _(u'Another XRandR GUI')
-COPYRIGHT = u'© chrysn 2008 – 2010'
+COPYRIGHT = u'© chrysn 2008 – 2011'
 
 TRANSLATORS = [
         'chrysn <chrysn@fsfe.org>',
