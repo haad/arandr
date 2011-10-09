40d7c96992292e59ba2ce881f5e6301ff4f96b6a
chrysn <chrysn@fsfe.org>
2011-10-09 15:24:31 +0200

    ready for release 0.1.5

--- screenlayout/meta.py
+++ screenlayout/meta.py
@@ -19,7 +19,7 @@
 import gettext
 gettext.install('arandr')
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 PROGRAMNAME = _(u'ARandR Screen Layout Editor')
 ## translators, please translate in the style of "Another XRandR GUI
 ## (ein weiteres GUI für XRandR)" so users get both the explanation of
